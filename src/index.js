7b936139df568525c9e96dd736b76e5b2ef3d77f
Roman Amici <amicir@gmail.com>
2020-05-09 14:03:07 -0600

    Works as a regression thing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-import * as tf from '@tensorflow/tfjs';
+import * as tf from "@tensorflow/tfjs";
+import { PINNBaseCore } from "./PINNBaseCore";
 
-function run () {
-    console.log("run");
-}
+const omega = 2 * Math.PI;
 
-document.addEventListener("DOMContentLoaded", run)
+const t = tf.reshape(tf.linspace(0, 1, 128), [128, 1]);
+const u = tf.sin(t.mul(omega));
+
+const lowerBounds = tf.tensor1d([0]);
+const upperBounds = tf.tensor1d([1]);
+
+const model = new PINNBaseCore(
+  [1, 8, 8, 8, 8, 1],
+  lowerBounds,
+  upperBounds,
+  omega
+);
+
+model.trainAdam(t, t, u, 1000);
+
+const u_hat = model.predict(t);
+
+const mse = tf.losses.meanSquaredError(u_hat, u);
